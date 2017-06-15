c2e8d973ec238ab378a11b272a221d5ab539d740
kelmankenberg <kellymank@gmail.com>
2017-06-15 17:05:45 -0500

    first commit

---
+++ resources/css/main.js
@@ -0,0 +1,24 @@
+// $(document).ready(function () {
+//   // $('.carousel').slick({
+//   //   arrows: false,
+//   //   autoplay: true,
+//   //   autoplaySpeed: 2000,
+//   //   fade: true
+//   // });
+//   $('.carousel').slick({
+//     // slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 4000,
+//     arrows: false,
+//     draggable: false,
+//     pauseOnFocus: false,
+//     pauseOnHover: true
+//   });
+// });
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    slidesToScroll: 1
+  });
+});
