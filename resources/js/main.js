9488688dded4f81e05b6aac29038051f652116e3
kelmankenberg <kellymank@gmail.com>
2017-06-16 10:57:53 -0500

    Finishing touches

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,28 +1,29 @@
-// $(document).ready(function () {
-//   // $('.carousel').slick({
-//   //   arrows: false,
-//   //   autoplay: true,
-//   //   autoplaySpeed: 2000,
-//   //   fade: true
-//   // });
-//   $('.carousel').slick({
-//     // slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 4000,
-//     arrows: false,
-//     draggable: false,
-//     pauseOnFocus: false,
-//     pauseOnHover: true
-//   });
-// });
 
 $(document).ready(function(){
   $('.carousel').slick({
     autoplay: true,
     slidesToScroll: 1,
     draggable: false,
-    pauseOnHover: true,
-    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    autoplaySpeed: 4000,
     arrows: false // Get rid of that pesky horizontal scrollbar
+  });
+
+
+// https://api.jquery.com/scroll/
+// https://api.jquery.com/animate/
+
+  var scrolledFromTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - scrolledFromTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scrolledFromTop = scrollTop;
+    } else if (scrolledFromTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      scrolledFromTop = scrollTop;
+    }
   });
 });
